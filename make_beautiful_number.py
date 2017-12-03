d0782a55c14761ff30f75f0645585930e10fb31f
samstudy <samat.study@gmail.com>
2017-12-03 14:06:04 +0600

    Added behavior on case if connection broke or DBAPI error

--- make_beautiful_number.py
+++ make_beautiful_number.py
@@ -1,7 +1,7 @@
 import phonenumbers, time
 from phonenumbers import carrier
 from create_local_db import destination_session,engine
-from sqlalchemy import MetaData,Table, Column, Integer
+from sqlalchemy import exc, MetaData,Table, Column, Integer
 
 SLEEP_TIME = 180
 metadata = MetaData()
@@ -14,14 +14,19 @@ updated_orders = Table('updated_orders',metadata,
 
 def make_beautiful_number():
     connection = engine.connect()
-    transaction = connection.begin()
-    target_orders = destination_session.query(updated_orders).filter(updated_orders.c.upd_number.is_(None))
-    for order in target_orders:
-        upd_number = phonenumbers.parse(order.contact_phone, "RU").national_number
-        inserted_number = updated_orders.update().values(upd_number = upd_number).\
+    try:
+        transaction = connection.begin()
+        target_orders = destination_session.query(updated_orders).filter(updated_orders.c.upd_number.is_(None))
+        for order in target_orders:
+            upd_number = phonenumbers.parse(order.contact_phone, "RU").national_number
+            inserted_number = updated_orders.update().values(upd_number = upd_number).\
                                                   where(updated_orders.c.contact_phone == order.contact_phone)
-        connection.execute(inserted_number)
-    transaction.commit()
+            connection.execute(inserted_number)
+        transaction.commit()
+        connection.close()
+    except exc.DBAPIError:
+        if connection.connection_invalidated:
+            connection = engine.connect()
     time.sleep(SLEEP_TIME)
 
 
